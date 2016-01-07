3cd222c4f517242bd5c54fdc9cb03a9617a6750e
ngocdo <nguyenanhngoc.do@gmail.com>
2016-01-07 17:39:06 +0000

    Partial form

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -21,6 +21,19 @@ class ChefsController < ApplicationController
         end
     end
     
+    def edit
+        @chef = Chef.find(params[:id])
+    end
+
+    def update
+        @chef = Chef.find(params[:id])
+        if @chef.update(chef_params)
+            flash[:success] = "Your chef was updated successfully!"
+            redirect_to chef_path(@chef)
+        else
+            render :edit        
+        end
+    end
     private
         def chef_params
            params.require(:chef).permit(:chefname, :email) 
