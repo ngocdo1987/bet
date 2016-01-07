1beab2930188568bd740e7d8e5a06e89bf66fa00
ngocdo <nguyenanhngoc.do@gmail.com>
2016-01-07 18:00:30 +0000

    Pagination

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,6 +1,6 @@
 class ChefsController < ApplicationController
     def index
-        @chefs = Chef.all
+        @chefs = Chef.paginate(page: params[:page], per_page: 5)
     end
     
     def show
