c240dabda363dae4bc2d2a517ee2bd786d0b7869
ngocdo <nguyenanhngoc.do@gmail.com>
2016-01-10 06:22:49 +0000

    Add 3 odds for match in CRUD

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,6 +1,6 @@
 class MatchesController < ApplicationController
     def index
-        @matches = Match.paginate(page: params[:page], per_page: 20)   
+        @matches = Match.paginate(page: params[:page], per_page: 20)
     end
    
     def show
@@ -18,11 +18,20 @@ class MatchesController < ApplicationController
     def create
         @match = Match.new(match_params)
         @leagues = League.all
-        @odd_spread = OddSpread.new
-        @odd_money_line = OddMoneyLine.new
-        @odd_total_point = OddTotalPoint.new
         
         if @match.save
+            @odd_spread = OddSpread.new(odd_spread_params[:odd_spreads])
+            @odd_spread.match = @match
+            @odd_spread.save
+            
+            @odd_money_line = OddMoneyLine.new(odd_money_line_params[:odd_money_lines])
+            @odd_money_line.match = @match
+            @odd_money_line.save
+            
+            @odd_total_point = OddTotalPoint.new(odd_total_point_params[:odd_total_points])
+            @odd_total_point.match = @match
+            @odd_total_point.save
+                
             flash[:success] = "Your match was created successfully!"
             redirect_to matches_path
         else
@@ -36,16 +45,28 @@ class MatchesController < ApplicationController
         @odd_spread = OddSpread.new
         @odd_money_line = OddMoneyLine.new
         @odd_total_point = OddTotalPoint.new
+        @odd_spreads = @match.odd_spreads.order(created_at: :desc)
+        @odd_money_lines = @match.odd_money_lines.order(created_at: :desc)
+        @odd_total_points = @match.odd_total_points.order(created_at: :desc)
     end
    
     def update
         @match = Match.find(params[:id])
         @leagues = League.all
-        @odd_spread = OddSpread.new
-        @odd_money_line = OddMoneyLine.new
-        @odd_total_point = OddTotalPoint.new
         
         if @match.update(match_params)
+            @odd_spread = OddSpread.new(odd_spread_params[:odd_spreads])
+            @odd_spread.match = @match
+            @odd_spread.save
+            
+            @odd_money_line = OddMoneyLine.new(odd_money_line_params[:odd_money_lines])
+            @odd_money_line.match = @match
+            @odd_money_line.save
+            
+            @odd_total_point = OddTotalPoint.new(odd_total_point_params[:odd_total_points])
+            @odd_total_point.match = @match
+            @odd_total_point.save
+            
             flash[:success] = "Your match was updated successfully!"
             redirect_to match_path(@match)
         else
@@ -56,7 +77,18 @@ class MatchesController < ApplicationController
     private
         def match_params
             params.require(:match).permit(:league_id, :home_team, :away_team, :home_number, :away_number, :home_score, :away_score, :match_time, :status, :calculated, :active)
-        end    
-           
-    
+        end  
+        
+        def odd_spread_params
+            params.require(:match).permit(odd_spreads: [:home_hdc, :away_hdc, :home_odd, :away_odd])    
+        end
+        
+        def odd_money_line_params
+            params.require(:match).permit(odd_money_lines: [:home_odd, :away_odd])
+        end
+        
+        def odd_total_point_params
+            params.require(:match).permit(odd_total_points: [:over_hdc, :under_hdc, :over_odd, :under_odd])    
+        end
+        
 end
